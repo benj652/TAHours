462aa998672d8b18c6389d378a1e79588e695a43
benj <benjaminjaffe8@gmail.com>
2024-11-12 20:23:52 -0500

    Updated user struct and made ta_queue struct

--- backend/models/ta_queue.go
+++ backend/models/ta_queue.go
@@ -1,1 +1,15 @@
 package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type TAQueue struct {
+	ID         primitive.ObjectID   `json:"_id,omitempty" bson:"_id,omitempty"`
+	TAs        []primitive.ObjectID `json:"TAs"`
+	Class      string               `json:"class"`
+	Directions string               `json:"directions"`
+	Tickets    []primitive.ObjectID `json:"tickets"`
+}
+
+func (t *TAQueue) TableName() string {
+	return "ta_queues"
+}
--- backend/models/user.go
+++ backend/models/user.go
@@ -3,11 +3,13 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	FirstName string             `json:"firstName"`
-	LastName  string             `json:"lastName"`
-	Email     string             `json:"email"`
-	Roles     []string           `json:"roles"`
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	FirstName   string             `json:"firstName"`
+	LastName    string             `json:"lastName"`
+	Email       string             `json:"email"`
+	ProfilePic  string             `json:"profilePic"`
+	Description string             `json:"description"`
+	Roles       []string           `json:"roles"`
 }
 
 func (u *User) TableName() string {
