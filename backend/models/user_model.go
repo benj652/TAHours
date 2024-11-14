d6fe9aa2909c0545a56695e23a880280758a6d6a
benj <benjaminjaffe8@gmail.com>
2024-11-14 12:44:50 -0500

    Added role model and renamed models files to "*_model.go"

--- backend/models/user_model.go
+++ backend/models/user_model.go
@@ -4,6 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	AccessToken string             `json:"accessToken"`
 	FirstName   string             `json:"firstName"`
 	LastName    string             `json:"lastName"`
 	Email       string             `json:"email"`
