6cde0d0198713cdd8e699a5bf0433c0e0a286912
benj652 <benjaminjaffe8@gmail.com>
2025-02-11 14:17:22 -0500

    Got TypeScript modular imports set up and made a Tw-merge/clsx utility class

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { cn } from "@/utils";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  return <div className="text-blue-500 text-3xl">bruh</div>;
+    return (
+        <div className={cn("btn btn-neutral text-blue-500 text-3xl")}>bruh</div>
+    );
 }
 
 export default App;
