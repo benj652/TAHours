0c108e15254ccf8792e4171cedd6259d0eb3f739
benj652 <benjaminjaffe8@gmail.com>
2025-02-11 20:29:12 -0500

    changed the port to 3000

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,10 +5,13 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src"), // Alias @ to the src folder
-        },
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Alias @ to the src folder
     },
+  },
 });
