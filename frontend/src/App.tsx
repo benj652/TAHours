01d11068453635ab011bc365381a7568f5928d91
voteme4 <zachberg2004>
2025-02-11 20:05:06 -0500

    Added basic file for each page and implemented url router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
-import { cn } from "@/utils";
+import { Route, Routes } from "react-router-dom";
+import AnalyticsPage from "./pages/analyticsPage/AnalyticsPage";
+import FeedPage from "./pages/feedPage/FeedPage";
+import LoginPage from "./pages/loginPage/LoginPage";
+import MainPage from "./pages/mainPage/MainPage";
+import ProfilePage from "./pages/profilePage/ProfilePage";
 
 function App() {
 
     return (
-        <div className={cn("btn btn-neutral text-blue-500 text-3xl")}>bruh</div>
+        <div>
+            <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/main" element={<MainPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/feed" element={<FeedPage />} />
+                <Route path="/analytics" element={<AnalyticsPage />} />
+            </Routes>
+        </div>
     );
 }
 
