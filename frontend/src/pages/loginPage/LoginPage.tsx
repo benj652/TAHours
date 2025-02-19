a8c075ecf1ee0d012f42b45b0bf7ac6f2db50df8
benj652 <benjaminjaffe8@gmail.com>
2025-02-18 19:27:55 -0500

    Got UI mockup for login page

--- frontend/src/pages/loginPage/LoginPage.tsx
+++ frontend/src/pages/loginPage/LoginPage.tsx
@@ -1,5 +1,16 @@
+import ColbyText from "../../assets/colbytext.svg";
+import ColbySeal from "../../assets/colbyseal.svg";
+
 export const Login = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <div className="flex h-screen justify-center items-center">
+      <div className="card bg-base-300 rounded-box flex flex-col items-center p-8 space-y-10">
+        <div className="flex flex-row">
+          <img src={ColbySeal} alt="Colby Seal" className="h-100 w-100" />
+          <img src={ColbyText} alt="Colby Seal" className="h-100 w-100" />
+        </div>
+        <h1 className="text-primary text-4xl">TA Hours</h1>
+      </div>
+    </div>
+  );
+};
