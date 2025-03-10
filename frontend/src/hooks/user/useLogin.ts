d025843674925bde2dd09e1bb487117114bb3a28
voteme4 <zachberg2004@gmail.com>
2025-03-10 19:59:22 -0400

    User Toast MMMMMM. success toasts are never visible because of page reload but include because it's fun

--- frontend/src/hooks/user/useLogin.ts
+++ frontend/src/hooks/user/useLogin.ts
@@ -2,58 +2,66 @@ import { TokenConfig, UserRoutes } from "@/types";
 import { httpClient } from "@/utils";
 import { CredentialResponse } from "@react-oauth/google";
 import { useState } from "react";
+import { toast } from "sonner";
 
 /**
-* Hook to login a user
-*
-* sets a bunch of item stuiff
+ * Hook to login a user
+ *
+ * sets a bunch of item stuiff
  */
 export const useLogin = () => {
-    // react hooks to set the loading and error states
-    const [loading, setLoading] = useState<boolean>(false);
-    const [error, setError] = useState<string | null>(null);
+  // react hooks to set the loading and error states
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
 
-    /**
-     * Login function to log hte user in
-     *
-    * @param jwtResponse - The response from the google login
-    *
-    * Pretty cool
-     */
-    const login = async (jwtResponse : CredentialResponse) => {
-        setLoading(true);
-        setError(null);
-        try {
-            // Do login stuff
-            //
-            // Gets the JWT response from the google login
-            const JWTR = jwtResponse.credential 
+  /**
+   * Login function to log hte user in
+   *
+   * @param jwtResponse - The response from the google login
+   *
+   * Pretty cool
+   */
+  const login = async (jwtResponse: CredentialResponse) => {
+    setLoading(true);
+    setError(null);
+    try {
+      // Do login stuff
+      //
+      // Gets the JWT response from the google login
+      const JWTR = jwtResponse.credential;
 
-            // Sets the JWT response in the local storage
-            localStorage.setItem(TokenConfig.UserJWTResponseToken, JWTR as string);
+      // Sets the JWT response in the local storage
+      localStorage.setItem(TokenConfig.UserJWTResponseToken, JWTR as string);
 
-            // makes a post request to the server to get or create the user
-            const res = await httpClient.post(UserRoutes.GetOrCreateUser)
-            
-            // Sets the user items in the local storage
-            localStorage.setItem(TokenConfig.UserTimeoutToken, Date.now().toString());
-            // const res = await httpClient.post(uriRoutes.getOrCreateUser, {}, {
-            //     headers: {
-            //        Authorization: `Bearer ${JWTR}`
-            //     }
-            // });
-            //
-            // sets the user items in the local storage
-            localStorage.setItem(TokenConfig.UserItemsToken, JSON.stringify(res.data));
+      // makes a post request to the server to get or create the user
+      const res = await httpClient.post(UserRoutes.GetOrCreateUser);
 
-            // reloads the page
-            window.location.reload();
-        } catch (error) {
-            // Capture and store any errors encountered during the request
-            setError(error instanceof Error ? error.message : "An error occurred");
-        } finally {
-            setLoading(false); // When all is set and done, stop loading
-        }
-    };
-    return { loading, login, error, setError };
+      // Sets the user items in the local storage
+      localStorage.setItem(TokenConfig.UserTimeoutToken, Date.now().toString());
+      // const res = await httpClient.post(uriRoutes.getOrCreateUser, {}, {
+      //     headers: {
+      //        Authorization: `Bearer ${JWTR}`
+      //     }
+      // });
+      //
+      // sets the user items in the local storage
+      localStorage.setItem(
+        TokenConfig.UserItemsToken,
+        JSON.stringify(res.data)
+      );
+
+      // is never visible but why not
+      toast.success("Successfully logged in");
+
+      // reloads the page
+      window.location.reload();
+    } catch (error) {
+      toast.error(error instanceof Error ? error.message : "An error occurred");
+      // Capture and store any errors encountered during the request
+      setError(error instanceof Error ? error.message : "An error occurred");
+    } finally {
+      setLoading(false); // When all is set and done, stop loading
+    }
+  };
+  return { loading, login, error, setError };
 };
--- frontend/src/hooks/user/useLogout.ts
+++ frontend/src/hooks/user/useLogout.ts
@@ -1,6 +1,7 @@
 import { TokenConfig } from "@/types";
 import { googleLogout } from "@react-oauth/google";
 import { useState } from "react";
+import { toast } from "sonner";
 
 /**
  * Custom hook: useLogout
@@ -14,40 +15,44 @@ import { useState } from "react";
  *  Pretty epic if i do say so myself
  */
 export const useLogout = () => {
-    // State to track whether the API request is in progress
-    const [loading, setLoading] = useState<boolean>(false); // Indicate that a request is in progress
-    // State to store any error messages encountered during the request
-    const [error, setError] = useState<string | null>(null);
-    /**
-     * Logout function to log the user out
-     *
-     * This function logs the user out by removing the user's token from local storage.
-     * It also calls the googleLogout function to log the user out of Google.
-     *
-     * It gets rid of all the trash I put in the local storqage
-     *
-     * Acctual garbage code and auth system at the moment will fix later
-     */
-    const logout = async () => {
-        setLoading(true); // Indicate that a request is in progress
-        setError(null);
-        try {
-            // Do logout stuff
-            googleLogout(); // Log the user out of Google
+  // State to track whether the API request is in progress
+  const [loading, setLoading] = useState<boolean>(false); // Indicate that a request is in progress
+  // State to store any error messages encountered during the request
+  const [error, setError] = useState<string | null>(null);
+  /**
+   * Logout function to log the user out
+   *
+   * This function logs the user out by removing the user's token from local storage.
+   * It also calls the googleLogout function to log the user out of Google.
+   *
+   * It gets rid of all the trash I put in the local storqage
+   *
+   * Acctual garbage code and auth system at the moment will fix later
+   */
+  const logout = async () => {
+    setLoading(true); // Indicate that a request is in progress
+    setError(null);
+    try {
+      // Do logout stuff
+      googleLogout(); // Log the user out of Google
 
-            localStorage.removeItem(TokenConfig.UserItemsToken); // Remove the user's items from local storage
+      localStorage.removeItem(TokenConfig.UserItemsToken); // Remove the user's items from local storage
 
-            localStorage.removeItem(TokenConfig.UserTimeoutToken); // Remove the user's timeout from local storage
+      localStorage.removeItem(TokenConfig.UserTimeoutToken); // Remove the user's timeout from local storage
 
-            localStorage.removeItem(TokenConfig.UserJWTResponseToken); // Remove the user's JWT response from local storage
+      localStorage.removeItem(TokenConfig.UserJWTResponseToken); // Remove the user's JWT response from local storage
 
-            window.location.reload(); // Reload the page to reflect the logout
-        } catch (error) {
-            // Capture and store any errors encountered during the request
-            setError(error instanceof Error ? error.message : "An error occurred");
-        } finally {
-            setLoading(false); // When all is set and done, stop loading
-        }
-    };
-    return { loading, logout, error }; // Return the state values and function for use in components
+      // is never visible but why not
+      toast.success("Logged out successfully");
+
+      window.location.reload(); // Reload the page to reflect the logout
+    } catch (error) {
+      toast.error(error instanceof Error ? error.message : "An error occurred");
+      // Capture and store any errors encountered during the request
+      setError(error instanceof Error ? error.message : "An error occurred");
+    } finally {
+      setLoading(false); // When all is set and done, stop loading
+    }
+  };
+  return { loading, logout, error }; // Return the state values and function for use in components
 };
