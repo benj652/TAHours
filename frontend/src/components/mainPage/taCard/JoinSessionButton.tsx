4549b0b097b5be100dfd3add7ba864c78d5f7782
voteme4 <zachberg2004@gmail.com>
2025-04-29 19:25:11 -0400

    Hides join session button for student

--- frontend/src/components/mainPage/taCard/JoinSessionButton.tsx
+++ frontend/src/components/mainPage/taCard/JoinSessionButton.tsx
@@ -1,43 +1,44 @@
 import { useJoinTaQueue } from "@/hooks";
 import { authStore, taQueueStore } from "@/store";
+import { Role } from "@/types";
 import { ObjectId } from "mongodb";
 // import { SessionButtonProps } from "@/types";
 
 type JoinSessionButtonProps = {
-    taQueueId: ObjectId| undefined;
-    // setTaQueues: (taQueues: TaQueue[]) => void;
-    };
+  taQueueId: ObjectId | undefined;
+  // setTaQueues: (taQueues: TaQueue[]) => void;
+};
 export const JoinSessionButton: React.FC<JoinSessionButtonProps> = ({
   taQueueId,
   // setTaQueues,
 }) => {
   const { error, joinTaQueue, loading } = useJoinTaQueue();
   const { userItems } = authStore();
-  const { allTaQueues, setAllTaQueues: setTaQueues} = taQueueStore();
+  const { allTaQueues, setAllTaQueues: setTaQueues } = taQueueStore();
   const handleJoinSession = async () => {
     if (!taQueueId) return;
     if (!userItems?._id) return;
     await joinTaQueue(taQueueId);
     if (error) return;
 
-const queue = allTaQueues.find((q) => q._id === taQueueId);
-  if (!queue) return;
+    const queue = allTaQueues.find((q) => q._id === taQueueId);
+    if (!queue) return;
 
-  const alreadyInQueue = queue.TAs.includes(userItems._id);
-  if (alreadyInQueue) return;
+    const alreadyInQueue = queue.TAs.includes(userItems._id);
+    if (alreadyInQueue) return;
 
-  // Create new updated TA queues immutably
-  const updatedQueues = allTaQueues.map((q) => {
-    if (q._id === taQueueId) {
-      return {
-        ...q,
-        TAs: [...q.TAs, userItems._id],
-      };
-    }
-    return q;
-  });
+    // Create new updated TA queues immutably
+    const updatedQueues = allTaQueues.map((q) => {
+      if (q._id === taQueueId) {
+        return {
+          ...q,
+          TAs: [...q.TAs, userItems._id],
+        };
+      }
+      return q;
+    });
 
-  setTaQueues(updatedQueues)
+    setTaQueues(updatedQueues);
 
     // setCurTas([...curTas, userItems._id]);
     // const curTaQueue = allTaQueues.filter(
@@ -49,6 +50,7 @@ const queue = allTaQueues.find((q) => q._id === taQueueId);
     // if (curUserInTaQueue.length > 0) return;
     // curTaQueue[0].TAs.push(userItems._id);
   };
+  if (userItems?.roles === Role.Student) return null;
   return (
     <button
       onClick={handleJoinSession}
