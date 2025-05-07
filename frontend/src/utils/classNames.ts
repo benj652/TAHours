ce79843daec94b0b16113878483bb027ad0aaa69
voteme4 <zachberg2004@gmail.com>
2025-05-06 20:25:05 -0400

    Documentation for frontend utils

--- frontend/src/utils/classNames.ts
+++ frontend/src/utils/classNames.ts
@@ -1,7 +1,18 @@
+/*
+ * A utility function to merge tailwind classes together.
+ * It takes in a variable amount of strings and uses `clsx` to merge them.
+ * Then it passes the merged string to `twMerge` to remove duplicates.
+ */
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+/**
+ * A utility function to merge tailwind classes together.
+ * It takes in a variable amount of strings and uses `clsx` to merge them.
+ * Then it passes the merged string to `twMerge` to remove duplicates.
+ * @param inputs - The strings to merge.
+ * @returns The merged string.
+ */
 export function cn(...inputs: string[]) {
   return twMerge(clsx(inputs));
 }
-
